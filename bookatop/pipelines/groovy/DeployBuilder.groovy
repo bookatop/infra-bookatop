f99c20ab183e3901255c3a7c65dec5f8068e1cc4
chudnikau <chudnikau@gmail.com>
2023-05-28 18:38:48 +0300

    Update structure of files

---
+++ bookatop/pipelines/groovy/DeployBuilder.groovy
@@ -0,0 +1,51 @@
+import ServiceDeployUtils
+
+def deployBackEndOnDev(
+        String branchName,
+        String serviceName,
+        String daemonName,
+        Integer buildNumber,
+        String sourcePath,
+        String destPath
+) {
+
+    node {
+        deployUtilsModule = load 'bookatop/pipelines/groovy/ServiceDeployUtils.groovy'
+
+        println branchName
+        println serviceName
+        println daemonName
+        println buildNumber
+        println sourcePath
+        println destPath
+
+    }
+
+//        utils.checkServiceExists(serviceName)
+//        utils.stopService(serviceName)
+//        utils.stopDaemon(daemonName)
+//        utils.replaceService(serviceName, buildNumber)
+//        utils.launchDaemon(daemonName)
+//        utils.checkServiceStatus(serviceName)
+}
+
+def deployBackEndOnProd(
+        String branchName,
+        String serviceName,
+        String daemonName,
+        Integer buildNumber,
+        String sourcePath,
+        String destPath
+) {
+
+}
+
+def deployFrontEndOnDev() {
+
+}
+
+def deployFrontEndOnProd() {
+
+}
+
+return this
