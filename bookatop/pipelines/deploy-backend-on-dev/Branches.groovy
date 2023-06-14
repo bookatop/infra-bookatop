bf5359c730ab3eda23f56fbec72b5594806d2bc0
chudnikau <chudnikau@gmail.com>
2023-06-14 12:58:01 +0300

    Modify active choose parameter

--- bookatop/pipelines/deploy-backend-on-dev/Branches.groovy
+++ bookatop/pipelines/deploy-backend-on-dev/Branches.groovy
@@ -1,4 +1,4 @@
 def branches = ("ls '/var/lib/jenkins/jobs/build-backend-on-dev/branches'").execute()
-branches.text.readLines().collect {
+return branches.text.readLines().collect {
     "'".concat(it.toLowerCase()).concat("'")
 }
